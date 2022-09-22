c8fc86ef8911137e56fc687683524a2ef64e901d
amleary888 <amleary888@gmail.com>
2022-09-22 12:45:21 +0100

    woo

---
+++ tourney-bot.py
@@ -0,0 +1,26 @@
+import discord
+import os
+import challonge
+
+challonge.set_credentials("L_eary", os.getenv('CHALLONGE'))
+
+class MyClient(discord.Client):
+
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        #print(f'Message from {message.author}: {message.content}')
+
+        if message.author != self.user:
+
+            if message.content.startswith(".$"):
+                if message.content.split(" ")[1] == "hi":
+                    await message.channel.send("Hello!")
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = MyClient(intents=intents)
+client.run(os.getenv('TOKEN'))
