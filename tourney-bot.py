0a1e331f4beed8d2ce8c8bd444bdf134b5d1d6f3
amleary888 <amleary888@gmail.com>
2022-09-23 11:51:15 +0100

    all

--- tourney-bot.py
+++ tourney-bot.py
@@ -3,7 +3,8 @@ import os
 import challonge
 import sqlite3
 
-conn = sqlite3.connect()
+conn = sqlite3.connect('clientInfo.db')
+c = conn.cursor()
 
 challonge.set_credentials("L_eary", os.getenv('CHALLONGE'))
 
@@ -21,6 +22,11 @@ class MyClient(discord.Client):
                 if message.content.split(" ")[1] == "hi":
                     await message.channel.send("Hello!")
 
+def createDataBase():
+    c.execute("""CREATE TABLE tournies (
+        tourneyID integer,
+        
+        )""")
 
 intents = discord.Intents.default()
 intents.message_content = True
